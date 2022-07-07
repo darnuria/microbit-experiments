bea6ae13157d88f180e73c9ed81c19b9f1749e94
Axel Viala <axel.viala@darnuria.eu>
2022-07-07 17:11:14 +0200

    Off course it's better with a cargo fmt.

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -6,13 +6,9 @@ use cortex_m_rt::entry;
 //use panic_halt as _;
 
 use panic_rtt_target as _;
-use rtt_target::{rtt_init_print, rprintln, rprint};
+use rtt_target::{rprint, rprintln, rtt_init_print};
 
-use microbit::{
-    board::Board,
-    display::blocking::Display,
-    hal::{prelude::*},
-};
+use microbit::{board::Board, display::blocking::Display, hal::prelude::*};
 
 const Y_MAX: usize = 4;
 const X_MAX: usize = 4;
@@ -22,38 +18,46 @@ fn main() -> ! {
     rtt_init_print!();
     rprintln!("Hello World from microbit v2");
     let mut matrix = [
-        [1,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ];
     let mut board = Board::take().unwrap();
 
-//    board.display_pins.col1.set_low().unwrap();
-//    board.display_pins.row1.set_high().unwrap();
-
     let mut timer = microbit::hal::Timer::new(board.TIMER0);
-// create the Display
-let mut display = Display::new(board.display_pins);
-    
+    // create the Display
+    let mut display = Display::new(board.display_pins);
+
     let mut x = 0;
     let mut y = 0;
     let mut x_iter = 1;
     let mut y_iter = 0;
     loop {
-        let old_x:i32 = x;
-        let old_y:i32 = y;
-
+        let old_x: i32 = x;
+        let old_y: i32 = y;
 
         display.show(&mut timer, matrix, 128);
         display.clear();
         timer.delay_ms(250u32);
         match (x as usize, y as usize) {
-            (0, 0) => { x_iter = 1; y_iter = 0 } ,
-            (X_MAX, 0) => { x_iter = 0; y_iter = 1 },
-            (X_MAX, Y_MAX) => { x_iter = -1; y_iter = 0 },
-            (0, Y_MAX) => { x_iter = 0; y_iter = -1 },
+            (0, 0) => {
+                x_iter = 1;
+                y_iter = 0
+            }
+            (X_MAX, 0) => {
+                x_iter = 0;
+                y_iter = 1
+            }
+            (X_MAX, Y_MAX) => {
+                x_iter = -1;
+                y_iter = 0
+            }
+            (0, Y_MAX) => {
+                x_iter = 0;
+                y_iter = -1
+            }
             (_, _) => {}
         };
         x += x_iter;
