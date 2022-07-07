2f75896914eac685c955b7542237f32bc8b1b622
Axel Viala <axel.viala@darnuria.eu>
2022-07-07 11:46:12 +0200

    Done with led-roulette.
    The assignment was fun, learned a little bit about pushing code to an ARM board.

---
+++ 05-led-roulette/main.rs
@@ -0,0 +1,65 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;
+//use panic_halt as _;
+
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln, rprint};
+
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*},
+};
+
+const Y_MAX: usize = 4;
+const X_MAX: usize = 4;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello World from microbit v2");
+    let mut matrix = [
+        [1,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+        [0,0,0,0,0],
+    ];
+    let mut board = Board::take().unwrap();
+
+//    board.display_pins.col1.set_low().unwrap();
+//    board.display_pins.row1.set_high().unwrap();
+
+    let mut timer = microbit::hal::Timer::new(board.TIMER0);
+// create the Display
+let mut display = Display::new(board.display_pins);
+    
+    let mut x = 0;
+    let mut y = 0;
+    let mut x_iter = 1;
+    let mut y_iter = 0;
+    loop {
+        let old_x:i32 = x;
+        let old_y:i32 = y;
+
+
+        display.show(&mut timer, matrix, 128);
+        display.clear();
+        timer.delay_ms(250u32);
+        match (x as usize, y as usize) {
+            (0, 0) => { x_iter = 1; y_iter = 0 } ,
+            (X_MAX, 0) => { x_iter = 0; y_iter = 1 },
+            (X_MAX, Y_MAX) => { x_iter = -1; y_iter = 0 },
+            (0, Y_MAX) => { x_iter = 0; y_iter = -1 },
+            (_, _) => {}
+        };
+        x += x_iter;
+        y += y_iter;
+
+        matrix[old_y as usize][old_x as usize] = 0;
+        matrix[y as usize][x as usize] = 1;
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Some experiments with a microbitV2
+
+Board: BBC Microbit V2.2
+
+Based on the Discovery embedded Rust course.
+
+<https://github.com/rust-embedded/discovery/>
+
+At the moment follow this commit: c01022114684dfd5e00851b85ddf921e4d4e0f02
+
+`.cargo/config` file and `memory.x` is neccesary for making the memory image
+with an entrypoint.
+
+
+- 05 led-roulette: my solution
+
